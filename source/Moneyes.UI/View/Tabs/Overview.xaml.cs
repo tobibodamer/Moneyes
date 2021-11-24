6943dfd2f5ae1f56d22ebb00f42bcf6e2355ff53
Zigeunerich <Zigeunerich@gmail.com>
2021-11-24 01:52:50 +0100

    Rearrange category cards drag drop

--- source/Moneyes.UI/View/Tabs/Overview.xaml.cs
+++ source/Moneyes.UI/View/Tabs/Overview.xaml.cs
@@ -1,19 +1,17 @@
 ﻿using Moneyes.UI.ViewModels;
-using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Moneyes.UI.View
 {
@@ -27,15 +25,25 @@ namespace Moneyes.UI.View
             InitializeComponent();
 
             CategoryItems.SourceUpdated += CategoryItems_SourceUpdated;
-            
+
+            SwapCommand = new AsyncCommand<int[]>(async (indeces, ct) =>
+            {
+                (CategoryItems.ItemsSource as ObservableCollection<CategoryExpenseViewModel>)
+                    .Move(indeces[0], indeces[1]);
+            });
         }
+
+        /// <summary>
+        /// Command that swaps categories
+        /// </summary>
+        public ICommand SwapCommand { get; }
 
         private void CategoryItems_SourceUpdated(object sender, DataTransferEventArgs e)
         {
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(CategoryItems.ItemsSource);
 
             if (view == null) return;
-            view.SortDescriptions.Add(new SortDescription("TotalExpense", ListSortDirection.Ascending));            
+            view.SortDescriptions.Add(new SortDescription("TotalExpense", ListSortDirection.Ascending));
         }
     }
 }
